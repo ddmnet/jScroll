883ffb2fad53adcd6e8c3dce3bdc9fefedd85abf
Jack Slingerland <jack.slingerland@gmail.com>
2012-01-07 11:05:39 -0500

    Almost have native iOS5 scrolling working.  Needs a little more testing yet, and an override to force iScroll if that's what you really want.

--- src/jScroll.js
+++ src/jScroll.js
@@ -14,12 +14,14 @@
 *	});
 *	$("div").jScroll("remove");  //Removes iScroll from all elements in this set.
 *
+* Note:  If you're using iOS5, the only valid options are vScroll & hScroll.
+*
 * It's not 100% fool-proof though.  It still relies on you knowing how to use iScroll.  If
 * you have questions about that, or about possible options, check out: http://cubiq.org/iscroll-4
 *
 * @author Jack Slingerland (jacks@teamddm.com)
 * @link http://www.teamddm.com
-* @version 1.1
+* @version 1.2
 */
 var iScrollers = [];
 (function($) {
@@ -28,7 +30,7 @@ var iScrollers = [];
 			action = "scroll";
 
 		//Determine what action we should be taking.
-		if(typeof arguments[0] == "string") {
+		if(typeof arguments[0] === "string") {
 			action = arguments[0];
 			customOptions = arguments[1];
 		} else {
@@ -37,7 +39,7 @@ var iScrollers = [];
 
 		var options = $.extend({}, $.fn.jScroll.defaultOptions, customOptions);
 		return this.each(function() {
-			
+
 			//Determine the id.  If one exists, use that.  Otherwise, create one.
 			var id = $(this).attr("id");
 			if(id === undefined || id === "") {
@@ -45,22 +47,43 @@ var iScrollers = [];
 				$(this).attr("id", id);
 			}
 
-			//Check to see if we should be removing iScroll instances.
-			if(action === "remove" || options.remove === true) {  //options.remove is here for backwards compatibility
-				remove_scroller(id);
-			} else {
-				//Create the iScroll objects, but first make sure it doesn't already exist.
-				var iScrollersCount = iScrollers.length;
-				var found = false;
-				for(var i = 0; i < iScrollersCount; i++) {
-					if(iScrollers[i].id == id) {
-						remove_scroller(id);
-						break;
-					}
-				}
-				add_scroller(id, options);
-			}
+			//Check to see if we're on iOS 5 so we can use native scrolling.
+			if(is_ios_5()) {
 
+				var type = "";
+				if(options.hScroll && !options.vScroll) {
+					type = "horizontal";
+				} else if(!options.hScroll && options.vScroll) {
+					type = "vertical";
+				} else {
+					type = "both";
+				}
+
+				if(action === "remove" || options.remove === true) {
+					remove_native_scroller(id, type);
+				} else {
+					add_native_scroller(id, type);
+				}
+
+			} else {
+
+				//Check to see if we should be removing iScroll instances.
+				if(action === "remove" || options.remove === true) {
+					remove_scroller(id);
+				} else {
+					//Create the iScroll objects, but first make sure it doesn't already exist.
+					var iScrollersCount = iScrollers.length;
+					var found = false;
+					for(var i = 0; i < iScrollersCount; i++) {
+						if(iScrollers[i].id === id) {
+							remove_scroller(id);
+							break;
+						}
+					}
+					add_scroller(id, options);
+				}
+
+			}
 		});
 	};
 
@@ -79,15 +102,30 @@ var iScrollers = [];
 		useTransition : false,  //Performance mode!
 		onBeforeScrollStart: function (e) {
 			var target = e.target;
-			while (target.nodeType != 1) target = target.parentNode;
+			while (target.nodeType !== 1) {
+				target = target.parentNode;
+			}
 
-			if (target.tagName != 'SELECT' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA')
+			if (target.tagName !== 'SELECT' && target.tagName !== 'INPUT' && target.tagName !== 'TEXTAREA') {
 				e.preventDefault();
+			}
 		},
 		remove : false
 	};
 
 	/* Private functions */
+
+	function add_native_scroller(id, type) {
+		$el = $("#"+id+":first-child");
+		if(type === "horizontal") {
+			$el.css("overflow-x", "scroll");
+		} else if(type === "vertical") {
+			$el.css("overflow-y", "scroll");
+		} else {
+			$el.css("overflow", "scroll");
+		}
+		$el.css("-webkit-overflow-scrolling", "touch");
+	}
 
 	function add_scroller(id, options) {
 		setTimeout(function() {
@@ -106,9 +144,29 @@ var iScrollers = [];
 		return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 	}
 
+	function is_ios_5() {
+		if(navigator.userAgent.match(/OS 5_\d like Mac OS X/i)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function remove_native_scroller(id) {
+		$el = $("#"+id+":first-child");
+		if(type === "horizontal") {
+			$el.css("overflow-x", "");
+		} else if(type === "vertical") {
+			$el.css("overflow-y", "");
+		} else {
+			$el.css("overflow", "");
+		}
+		$el.css("-webkit-overflow-scrolling", "");
+	}
+
 	function remove_scroller(id) {
 		for(var i = 0; i < iScrollers.length; i++) {
-			if(iScrollers[i].id == id) {
+			if(iScrollers[i].id === id) {
 				if(iScrollers[i].instance !== null) {
 					iScrollers[i].instance.destroy();
 					iScrollers[i].instance = null;
